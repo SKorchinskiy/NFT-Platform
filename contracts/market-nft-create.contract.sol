356c02a258dac6243b2954a541fcf4fc8f3054cc
SKorchinskiy <ddphyk@gmail.com>
2024-04-22 23:21:41 +0300

    + add custom errors/events/modifiers to market-nft creational contract

--- contracts/market-nft-create.contract.sol
+++ contracts/market-nft-create.contract.sol
@@ -10,6 +10,48 @@ contract MarketNFTCreateContract is ERC721URIStorage {
     address payable owner;
     address contractAddress;
 
+    error InappropriateMintingFee(
+        uint256 expected_fee, 
+        uint256 received_fee
+    );
+    error OwnershipRejection(
+        string message
+    );
+
+    event TokenWrapperCreated(
+        uint256 token_id,
+        address token_owner,
+        string token_uri,
+        address contract_address
+    );
+    event TokenMinted(
+        uint256 token_id,
+        address token_owner,
+        string token_uri,
+        address contract_address
+    );
+    event ProfitWithdraw(
+        address receiver,
+        uint256 amount
+    );
+
+    modifier validateMintingFee() {
+        if (msg.value != minting_fee)
+            revert InappropriateMintingFee({
+                expected_fee: minting_fee,
+                received_fee: msg.value
+            });
+        _;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != owner)
+            revert OwnershipRejection({
+                message: "Only the owner of the contract can withdraw profit!"
+            });
+        _;
+    }
+
     constructor(address marketAddress) ERC721("SKorchinskiyMarket", "SKM") {
         contractAddress = marketAddress;
     }
@@ -21,30 +63,38 @@ contract MarketNFTCreateContract is ERC721URIStorage {
         _setTokenURI(token_id, tokenURI);
         setApprovalForAll(contractAddress, true);
 
+        emit TokenWrapperCreated({
+            token_id: token_id,
+            token_owner: msg.sender,
+            token_uri: tokenURI,
+            contract_address: contractAddress
+        });
         return token_id;
     }
 
-    function mint_token(string memory tokenURI) external payable returns(uint256) {
-        require(
-            msg.value == minting_fee,
-            "Minting fee should be equal to 0.0075 ether!"
-        );
-
+    function mint_token(string memory tokenURI) external payable validateMintingFee returns(uint256) {
         token_id++;
 
         _mint(msg.sender, token_id);
         _setTokenURI(token_id, tokenURI);
         setApprovalForAll(contractAddress, true);
 
+        emit TokenMinted({
+            token_id: token_id,
+            token_owner: msg.sender,
+            token_uri: tokenURI,
+            contract_address: contractAddress
+        });
         return token_id;
     }
 
-    function withdraw() external {
-        require(
-            msg.sender == owner,
-            "Only the owner of the contract can withdraw the profit!"
-        );
-
-        owner.transfer(address(this).balance);
+    function withdraw() external onlyOwner {
+        uint256 amount = address(this).balance; 
+        
+        owner.transfer(amount);
+        emit ProfitWithdraw({
+            receiver: msg.sender,
+            amount: amount
+        });
     }
 }
