a783ee779849ebc0b862cfe05363f614e0f1a0b4
SKorchinskiy <ddphyk@gmail.com>
2024-05-02 23:47:21 +0300

    + add nft card component

--- client/src/app/portal/_components/nft-card/nft-card.component.tsx
+++ client/src/app/portal/_components/nft-card/nft-card.component.tsx
@@ -1,4 +1,8 @@
 import Image from "next/image";
+import styles from "./nft-card.module.css";
+
+import { Fragment, useState } from "react";
+import NFTCardModal from "../nft-card-modal/nft-card-modal.component";
 
 export type NFT = {
   nft: {
@@ -11,11 +15,62 @@ export type NFT = {
 };
 
 export default function NFTCard({ nft }: NFT) {
-  return nft.image ? (
-    <div>
-      <Image src={nft.image} alt="nft" width={100} height={100}></Image>
-      <p>Name: {nft.name}</p>
-      <p>Description: {nft.description}</p>
-    </div>
-  ) : null;
+  const [isCardModalOpen, setIsCardModalOpen] = useState<boolean>(false);
+
+  const toggleIsCardModalOpen = () =>
+    setIsCardModalOpen((prevState) => !prevState);
+
+  return (
+    <Fragment>
+      <div className={styles["card-container"]} onClick={toggleIsCardModalOpen}>
+        <Image
+          src={nft.image}
+          alt="nft"
+          width={200}
+          height={200}
+          style={{
+            borderRadius: "10px 10px 0px 0px",
+          }}
+        />
+        <div
+          style={{
+            background: "red",
+            borderRadius: "0px 0px 10px 10px",
+          }}
+        >
+          <div
+            style={{
+              marginLeft: "15px",
+              fontFamily: "fantasy",
+              fontSize: "18px",
+            }}
+          >
+            <p>Owned by YOU</p>
+            <p>{nft.name}</p>
+            <p>{nft.description}</p>
+          </div>
+        </div>
+        <div className={styles["card-overlay"]} />
+      </div>
+      {isCardModalOpen ? (
+        <Fragment>
+          <NFTCardModal
+            nft={nft}
+            toggleIsCardModalOpen={toggleIsCardModalOpen}
+          />
+          <div
+            style={{
+              position: "absolute",
+              top: -100,
+              bottom: -100,
+              left: -100,
+              right: -100,
+              background: "rgba(0, 0, 0, 0.4)",
+              zIndex: 1,
+            }}
+          />
+        </Fragment>
+      ) : null}
+    </Fragment>
+  );
 }
