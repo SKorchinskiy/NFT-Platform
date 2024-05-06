a3fd212bcecb77a444e0063e5826e9f1d5ae7648
SKorchinskiy <ddphyk@gmail.com>
2024-05-06 19:50:38 +0300

    + add nft-purchase-details component

---
+++ client/src/app/portal/_components/nft-purchase-details/nft-purchase-details.component.tsx
@@ -0,0 +1,48 @@
+import { NFT } from "@/app/types/nft.type";
+import styles from "./nft-purchase-details.module.css";
+import { Status } from "../nft-card/nft-card.component";
+
+export default function NFTPurchaseDetails({ nft }: { nft: NFT }) {
+  return (
+    <div className={styles["nft-details-container"]}>
+      <div className={styles["nft-details-info"]}>
+        <p>
+          <b>Token ID:</b> {nft.token_id.toString()}
+        </p>
+        <p>
+          <b>Collection:</b> {nft.name}
+        </p>
+        <p>
+          <b>Description:</b> {nft.description}
+        </p>
+        <p>
+          <b>Status:</b> {Status[Number(nft.status)]}
+        </p>
+        <p>
+          <b>Seller:</b>{" "}
+          {(() => {
+            const head = nft.token_seller.slice(0, 5);
+            const tail = nft.token_seller.slice(nft.token_seller.length - 5);
+            return `${head}...${tail}`;
+          })()}
+        </p>
+        <p>
+          <b>Buyer:</b>{" "}
+          {(() => {
+            const head = nft.token_holder.slice(0, 5);
+            const tail = nft.token_holder.slice(nft.token_holder.length - 5);
+            return `${head}...${tail}`;
+          })()}
+        </p>
+        <p>
+          <b>Price:</b> {Number(nft.token_price) / 1e18} ETH
+        </p>
+      </div>
+      {/* <div className={styles["nft-interactive-container"]}>
+        <div className={styles["purchase-button"]} onClick={buyListedToken}>
+          Buy token
+        </div>
+      </div> */}
+    </div>
+  );
+}
