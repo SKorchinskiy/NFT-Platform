53ac2eb5d8033982c210f1201c3efa8ce60fee09
SKorchinskiy <ddphyk@gmail.com>
2024-05-06 19:51:23 +0300

    + update nft-card modal to support scrollability and purchased tokens details

--- client/src/app/portal/_components/nft-card-modal/nft-card-modal.component.tsx
+++ client/src/app/portal/_components/nft-card-modal/nft-card-modal.component.tsx
@@ -7,77 +7,81 @@ import { useContext } from "react";
 import { AddressContext } from "@/app/providers/address.provider";
 import NFTCancelationDetails from "../nft-cancelation-details/nft-cancelation-details.component";
 import NFTBuyingDetails from "../nft-buying-details/nft-buying-details.component";
+import NFTPurchaseDetails from "../nft-purchase-details/nft-purchase-details.component";
 
 type NFTModalProps = {
   nft: NFT;
+  isScrollable?: boolean;
   toggleIsCardModalOpen: Function;
   activeTokenHandler: Function;
 };
 
 export default function NFTCardModal({
   nft,
+  isScrollable = true,
   toggleIsCardModalOpen,
   activeTokenHandler,
 }: NFTModalProps) {
   const { address } = useContext(AddressContext);
 
   return (
-    <div className={styles["modal-container"]}>
-      <div
-        className={styles["atr-close"]}
-        onClick={(e) => toggleIsCardModalOpen()}
-      >
-        <Image src="/close-sign.png" alt="close" width={35} height={20} />
+    <div className={styles["modal-wrapper"]}>
+      <div className={styles["modal-container"]}>
+        <div
+          className={styles["atr-close"]}
+          onClick={(e) => toggleIsCardModalOpen()}
+        >
+          <Image src="/close-sign.png" alt="close" width={35} height={20} />
+        </div>
+        <div
+          style={{ display: isScrollable ? "block" : "none" }}
+          className={styles["atr-arrow-left"]}
+          onClick={(e) => activeTokenHandler(-1)}
+        >
+          <Image src="/arrow-left.png" alt="close" width={40} height={40} />
+        </div>
+        <div
+          style={{ display: isScrollable ? "block" : "none" }}
+          className={styles["atr-arrow-right"]}
+          onClick={(e) => activeTokenHandler(1)}
+        >
+          <Image src="/arrow-right.png" alt="close" width={40} height={40} />
+        </div>
+        <div className={styles["atr-nft-image"]}>
+          <Image
+            src={nft.image}
+            alt="nft"
+            width={400}
+            height={400}
+            style={{ borderRadius: "10px" }}
+          />
+        </div>
+        {(() => {
+          if (Number(nft.status) == Status.ACTIVE) {
+            return nft.token_seller == address ? (
+              <NFTCancelationDetails
+                nft={nft}
+                toggleIsCardModalOpen={toggleIsCardModalOpen}
+              />
+            ) : (
+              <NFTBuyingDetails
+                nft={nft}
+                toggleIsCardModalOpen={toggleIsCardModalOpen}
+              />
+            );
+          } else if (Number(nft.status) == Status.NONE) {
+            return (
+              <NFTListingDetails
+                nft={nft}
+                toggleIsCardModalOpen={toggleIsCardModalOpen}
+              />
+            );
+          } else if (Number(nft.status) == Status.SOLD) {
+            return <NFTPurchaseDetails nft={nft} />;
+          }
+          return <></>;
+        })()}
       </div>
-      <div
-        className={styles["atr-arrow-left"]}
-        onClick={(e) => activeTokenHandler(-1)}
-      >
-        <Image src="/arrow-left.png" alt="close" width={40} height={40} />
-      </div>
-      <div
-        className={styles["atr-arrow-right"]}
-        onClick={(e) => activeTokenHandler(1)}
-      >
-        <Image src="/arrow-right.png" alt="close" width={40} height={40} />
-      </div>
-      <div className={styles["atr-nft-image"]}>
-        <Image
-          src={nft.image}
-          alt="nft"
-          width={400}
-          height={400}
-          style={{ borderRadius: "10px" }}
-        />
-      </div>
-      {(() => {
-        if (
-          Number(nft.status) == Status.ACTIVE &&
-          nft.token_seller == address
-        ) {
-          return (
-            <NFTCancelationDetails
-              nft={nft}
-              toggleIsCardModalOpen={toggleIsCardModalOpen}
-            />
-          );
-        } else if (Number(nft.status) == Status.ACTIVE) {
-          return (
-            <NFTBuyingDetails
-              nft={nft}
-              toggleIsCardModalOpen={toggleIsCardModalOpen}
-            />
-          );
-        } else if (nft.token_seller == address) {
-          return (
-            <NFTListingDetails
-              nft={nft}
-              toggleIsCardModalOpen={toggleIsCardModalOpen}
-            />
-          );
-        }
-        return <></>;
-      })()}
     </div>
   );
 }
