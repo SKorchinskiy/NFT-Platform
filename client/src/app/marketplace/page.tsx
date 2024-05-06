f1138d1dfe952583559eb61acbb1377cc3e0037a
SKorchinskiy <ddphyk@gmail.com>
2024-05-06 19:53:13 +0300

    + add sections support for marketplace and portal pages

--- client/src/app/marketplace/page.tsx
+++ client/src/app/marketplace/page.tsx
@@ -4,8 +4,76 @@ import { useContext } from "react";
 import { TokensContext } from "../providers/nft-tokens.provider";
 import NFTCardList from "../portal/_components/nft-card-list/nft-card-list.component";
 
-export default function MarketplacePage() {
-  const { marketTokens } = useContext(TokensContext);
+import { Montserrat } from "next/font/google";
+import { Status } from "../portal/_components/nft-card/nft-card.component";
+import Carousel from "./_components/carousel/carousel.component";
 
-  return <NFTCardList nfts={marketTokens} />;
+const nunito = Montserrat({
+  subsets: ["latin"],
+  weight: "700",
+  style: "normal",
+});
+
+export default function MarketplacePage() {
+  const { marketTokens, purchasedTokens } = useContext(TokensContext);
+
+  return (
+    <div>
+      <Carousel nfts={marketTokens} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "rgba(0, 0, 0, 0.1)",
+          borderRadius: "10px",
+        }}
+      >
+        <p
+          style={{
+            fontSize: 36,
+            textTransform: "uppercase",
+            letterSpacing: 4,
+          }}
+          className={nunito.className}
+        >
+          <b>For sale</b>
+        </p>
+      </div>
+      <NFTCardList
+        nfts={marketTokens
+          .filter(
+            (token) => token.status.toString() == Status["ACTIVE"].toString()
+          )
+          .slice(0, 8)}
+      />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "rgba(0, 0, 0, 0.1)",
+          borderRadius: "10px",
+        }}
+      >
+        <p
+          style={{
+            fontSize: 36,
+            textTransform: "uppercase",
+            letterSpacing: 4,
+          }}
+          className={nunito.className}
+        >
+          <b>Recently Purchased</b>
+        </p>
+      </div>
+      <NFTCardList
+        nfts={purchasedTokens
+          .filter(
+            (token) => token.status.toString() == Status["SOLD"].toString()
+          )
+          .slice(0, 8)}
+      />
+    </div>
+  );
 }
--- client/src/app/portal/page.tsx
+++ client/src/app/portal/page.tsx
@@ -5,16 +5,101 @@ import { TokensContext } from "../providers/nft-tokens.provider";
 
 import NFTCardList from "./_components/nft-card-list/nft-card-list.component";
 
+import { Montserrat } from "next/font/google";
+import { Status } from "./_components/nft-card/nft-card.component";
+import { AddressContext } from "../providers/address.provider";
+
+const nunito = Montserrat({
+  subsets: ["latin"],
+  weight: "700",
+  style: "normal",
+});
+
 export default function Portal() {
-  const { tokens } = useContext(TokensContext);
+  const { tokens, marketTokens, purchasedTokens } = useContext(TokensContext);
+  const { address } = useContext(AddressContext);
 
   return tokens ? (
-    <div
-      style={{
-        position: "relative",
-      }}
-    >
-      <NFTCardList nfts={tokens} />
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "rgba(0, 0, 0, 0.1)",
+          borderRadius: "10px",
+        }}
+      >
+        <p
+          style={{
+            fontSize: 36,
+            textTransform: "uppercase",
+            letterSpacing: 4,
+          }}
+          className={nunito.className}
+        >
+          <b>Personal Tokens</b>
+        </p>
+      </div>
+      <NFTCardList nfts={tokens.slice(0, 8)} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "rgba(0, 0, 0, 0.1)",
+          borderRadius: "10px",
+        }}
+      >
+        <p
+          style={{
+            fontSize: 36,
+            textTransform: "uppercase",
+            letterSpacing: 4,
+          }}
+          className={nunito.className}
+        >
+          <b>Listed Tokens</b>
+        </p>
+      </div>
+      <NFTCardList
+        nfts={marketTokens
+          .filter(
+            (token) =>
+              token.token_seller == address &&
+              token.status.toString() == Status["ACTIVE"].toString()
+          )
+          .slice(0, 8)}
+      />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "rgba(0, 0, 0, 0.1)",
+          borderRadius: "10px",
+        }}
+      >
+        <p
+          style={{
+            fontSize: 36,
+            textTransform: "uppercase",
+            letterSpacing: 4,
+          }}
+          className={nunito.className}
+        >
+          <b>Recently Sold</b>
+        </p>
+      </div>
+      <NFTCardList
+        nfts={purchasedTokens
+          .filter(
+            (token) =>
+              token.token_seller == address &&
+              token.status.toString() == Status["SOLD"].toString()
+          )
+          .slice(0, 8)}
+      />
     </div>
   ) : null;
 }
