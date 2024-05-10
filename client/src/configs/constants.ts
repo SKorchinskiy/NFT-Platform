2808932e7fd49100c6a924ce549211e0d52ab430
SKorchinskiy <ddphyk@gmail.com>
2024-05-10 18:49:34 +0300

    + update constants

--- client/src/configs/constants.ts
+++ client/src/configs/constants.ts
@@ -1,4 +1,10 @@
+export const PINATA_KEY = "<YOUR PINATA API KEY>";
+
+export const PINATA_GATEWAY = "<YOUR PINATA GATEWAY>";
+
 export const contract_addresses = {
-  marketResellContract: "<RESELL CONTRACT ADDRESS>",
   nftCollectionContract: "<NFT COLLECTION CONTRACT ADDRESS>",
+  marketResellContract: "<RESELL CONTRACT ADDRESS>",
+  marketCreateContract: "<MARKET CREATE CONTRACT ADDRESS>",
+  nftCreateContract: "<NFT CREATE CONTRACT ADDRESS>",
 };
