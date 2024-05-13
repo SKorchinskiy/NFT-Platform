d6c9993ef6ce79ece889f1950b4587e413ddf2e1
SKorchinskiy <ddphyk@gmail.com>
2024-05-13 23:37:38 +0300

    + add type for trade tokens

---
+++ client/src/app/types/trade-tokens.type.ts
@@ -0,0 +1,9 @@
+export type TradeTokens = {
+  auction_end_time: BigInt;
+  auction_id: BigInt;
+  beneficiary: string;
+  highest_bid: BigInt;
+  highest_bidder: string;
+  nft_contract: string;
+  token_id: BigInt;
+};
