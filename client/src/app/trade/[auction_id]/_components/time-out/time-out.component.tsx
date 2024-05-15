cd8d7b15eabaf8962c2409fee38f9656a891730e
SKorchinskiy <ddphyk@gmail.com>
2024-05-15 16:07:16 +0300

    + separate time-out component styles from functional code

--- client/src/app/trade/[auction_id]/_components/time-out/time-out.component.tsx
+++ client/src/app/trade/[auction_id]/_components/time-out/time-out.component.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import styles from "./time-out.module.css";
 import { useEffect, useRef, useState } from "react";
 
 const TIME_CONV = {
@@ -15,15 +16,12 @@ export default function TimeOut({ end_time }: { end_time: number }) {
   );
   const tid = useRef<NodeJS.Timeout>();
 
-  console.log({ end_time, now: Math.floor(Date.now()) });
-
   useEffect(() => {
     if (!tid.current) {
       tid.current = setInterval(
         () => setTimeLeft((prevTime) => prevTime - 1000),
         1000
       );
-      console.log({ timeLeft });
     } else if (timeLeft === 0) {
       clearTimeout(tid.current);
     }
@@ -37,118 +35,40 @@ export default function TimeOut({ end_time }: { end_time: number }) {
   };
 
   return (
-    <div
-      style={{
-        width: 400,
-        marginTop: 50,
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "1fr 1fr",
-            columnGap: 5,
-          }}
-        >
+    <div className={styles["time-out-outer-container"]}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div className={styles["time-outer-container"]}>
           {Array.from(timeFormatter(timeLeft / TIME_CONV.day)).map(
             (value: string, index: number) => (
-              <div
-                key={index}
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: 70,
-                  width: 40,
-                  background: "black",
-                  color: "white",
-                }}
-              >
+              <div key={index} className={styles["time-container"]}>
                 {value}
               </div>
             )
           )}
         </div>
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "1fr 1fr",
-            columnGap: 5,
-          }}
-        >
+        <div className={styles["time-outer-container"]}>
           {Array.from(
             timeFormatter((timeLeft % TIME_CONV.day) / TIME_CONV.hour)
           ).map((value: string, index: number) => (
-            <div
-              key={index}
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: 70,
-                width: 40,
-                background: "black",
-                color: "white",
-              }}
-            >
+            <div key={index} className={styles["time-container"]}>
               {value}
             </div>
           ))}
         </div>
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "1fr 1fr",
-            columnGap: 5,
-          }}
-        >
+        <div className={styles["time-outer-container"]}>
           {Array.from(
             timeFormatter((timeLeft % TIME_CONV.hour) / TIME_CONV.minute)
           ).map((value: string, index: number) => (
-            <div
-              key={index}
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: 70,
-                width: 40,
-                background: "black",
-                color: "white",
-              }}
-            >
+            <div key={index} className={styles["time-container"]}>
               {value}
             </div>
           ))}
         </div>
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "1fr 1fr",
-            columnGap: 5,
-          }}
-        >
+        <div className={styles["time-outer-container"]}>
           {Array.from(
             timeFormatter((timeLeft % TIME_CONV.minute) / TIME_CONV.second)
           ).map((value: string, index: number) => (
-            <div
-              key={index}
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: 70,
-                width: 40,
-                background: "black",
-                color: "white",
-              }}
-            >
+            <div key={index} className={styles["time-container"]}>
               {value}
             </div>
           ))}
