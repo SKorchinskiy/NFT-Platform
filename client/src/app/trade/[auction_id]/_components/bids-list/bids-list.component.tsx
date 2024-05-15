206b3e4ce180df85a774ac0c856579c33187c81c
SKorchinskiy <ddphyk@gmail.com>
2024-05-15 16:07:00 +0300

    + separate bids-list component styles from functional code

--- client/src/app/trade/[auction_id]/_components/bids-list/bids-list.component.tsx
+++ client/src/app/trade/[auction_id]/_components/bids-list/bids-list.component.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import styles from "./bids-list.module.css";
+
 import { Bid as BidType } from "@/app/types/bid.type";
 import Bid from "../bid/bid.component";
 
@@ -9,20 +11,7 @@ type BidsListProps = {
 
 export default function BidsList({ bids }: BidsListProps) {
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "1fr 1fr",
-        columnGap: 10,
-        rowGap: 10,
-        height: 500,
-        overflow: "scroll",
-        background: "rgba(0, 0, 0, 0.4)",
-        padding: 10,
-        borderRadius: 5,
-        boxShadow: "10px 10px 10px rgba(0, 0, 0, 0.3)",
-      }}
-    >
+    <div className={styles["bid-list-container"]}>
       {bids.map((bid, index) => (
         <Bid key={index} bid={bid} />
       ))}
