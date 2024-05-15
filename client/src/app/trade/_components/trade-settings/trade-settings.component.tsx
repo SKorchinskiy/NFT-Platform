3b7cb764ad9f85cede4ffc0986aca78354101bae
SKorchinskiy <ddphyk@gmail.com>
2024-05-15 16:08:33 +0300

    + separate trade-settings component styles from functional code

--- client/src/app/trade/_components/trade-settings/trade-settings.component.tsx
+++ client/src/app/trade/_components/trade-settings/trade-settings.component.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from "./trade-settings.module.css";
 
 export type TRADE_OPTIONS = {
   trade_time: number;
@@ -13,34 +14,16 @@ export default function TradeSettings({
 }: {
   onTradePublish: Function;
 }) {
-  const [tradeOptions, setTradeOptions] = useState<TRADE_OPTIONS>(
-    DEFAULT_TRADE_OPTIONS
-  );
+  const [tradeOptions] = useState<TRADE_OPTIONS>(DEFAULT_TRADE_OPTIONS);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "space-around",
-        width: 600,
-        height: 500,
-        background: "blue",
-      }}
-    >
+    <div className={styles["trade-settings-container"]}>
       <input
         placeholder="Input auction time in seconds..."
-        style={{
-          width: 200,
-          padding: 15,
-        }}
+        style={{ width: 200, padding: 15 }}
       ></input>
       <button
-        style={{
-          width: 200,
-          padding: 15,
-        }}
+        style={{ width: 200, padding: 15 }}
         onClick={() => onTradePublish(tradeOptions)}
       >
         List for English Auction
