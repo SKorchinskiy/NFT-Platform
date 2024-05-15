8632f0ecfe46937154ebe254ec21f3a1f2ad7047
SKorchinskiy <ddphyk@gmail.com>
2024-05-15 16:04:24 +0300

    + separate action-popup component from styles

--- client/src/app/_components/action-popup/action-popup.component.tsx
+++ client/src/app/_components/action-popup/action-popup.component.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { PopupContext } from "@/app/providers/popup.provider";
+import styles from "./action-popup.module.css";
 import { useContext, useEffect, useState } from "react";
+
+import { PopupContext } from "@/app/providers/popup.provider";
 
 export default function ActionPopup() {
   const [isVisible, setIsVisible] = useState(false);
@@ -17,22 +19,8 @@ export default function ActionPopup() {
 
   return (
     <div
-      style={{
-        display: isVisible ? "block" : "none",
-        position: "absolute",
-        right: 50,
-        bottom: 50,
-        background: "rgba(179, 225, 172, 0.8)",
-        paddingLeft: 100,
-        paddingRight: 100,
-        paddingTop: 30,
-        paddingBottom: 30,
-        borderRadius: 5,
-        boxShadow: "10px 5px 10px rgba(0, 0, 0, 0.5)",
-        fontFamily: "monospace",
-        textTransform: "uppercase",
-        color: "black",
-      }}
+      className={styles["popup-container"]}
+      style={{ display: isVisible ? "block" : "none" }}
     >
       {text}
     </div>
