31a9b872f107eb69992e03df1784d86b5e0b982d
SKorchinskiy <ddphyk@gmail.com>
2024-05-15 16:05:37 +0300

    + separate mint page styles from functional code

--- client/src/app/mint/page.tsx
+++ client/src/app/mint/page.tsx
@@ -1,16 +1,9 @@
+import styles from "./page.module.css";
 import MintingForm from "./_components/minting-form/minting-form.component";
 
 export default function MintPage() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "flex-end",
-        width: "100%",
-        padding: 50,
-        boxSizing: "border-box",
-      }}
-    >
+    <div className={"minting-container"}>
       <MintingForm />
     </div>
   );
