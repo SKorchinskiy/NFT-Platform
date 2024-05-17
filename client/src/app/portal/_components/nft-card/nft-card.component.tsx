f1ec16c3683d649b23a00008ab46646041cdc93a
SKorchinskiy <ddphyk@gmail.com>
2024-05-18 00:22:13 +0300

    + add status-plate to nft card component

--- client/src/app/portal/_components/nft-card/nft-card.component.tsx
+++ client/src/app/portal/_components/nft-card/nft-card.component.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import styles from "./nft-card.module.css";
 
-import { Fragment, useContext } from "react";
+import { useContext } from "react";
 import { NFT } from "@/app/types/nft.type";
 import { AddressContext } from "@/app/providers/address.provider";
+import StatusPlate from "../status-plate/status-plate.component";
 
 export enum Status {
   NONE,
@@ -26,47 +27,46 @@ export default function NFTCard({
   const { address } = useContext(AddressContext);
 
   return (
-    <Fragment>
-      <div
-        className={styles["card-container"]}
-        onClick={(e) => {
-          initTokenHandler(nft.token_id);
-          toggleIsCardModalOpen();
-        }}
-      >
-        <Image
-          src={nft.image}
-          alt="nft"
-          width={250}
-          height={250}
-          style={{ borderRadius: "10px 10px 0px 0px" }}
-        />
-        <div className={styles["card-details-container"]}>
-          <div className={styles["card-details"]}>
-            <p>
-              <b>Owned by: </b>
-              {nft.token_seller == address
-                ? "YOU"
-                : (() => {
-                    const head = nft.token_seller.slice(0, 5);
-                    const tail = nft.token_seller.slice(address.length - 5);
-                    return `${head}...${tail}`;
-                  })()}
-            </p>
-            <p>
-              <b>Collection: </b> {nft.name}
-            </p>
-            <p>
-              <b>Description: </b> {nft.description}
-            </p>
-            <p>
-              <b>Price: </b>
-              {Number(nft.token_price) / 1e18} ETH
-            </p>
-          </div>
+    <div
+      className={styles["card-container"]}
+      onClick={(e) => {
+        initTokenHandler(nft.token_id);
+        toggleIsCardModalOpen();
+      }}
+    >
+      <Image
+        src={nft.image}
+        alt="nft"
+        width={250}
+        height={250}
+        style={{ borderRadius: "10px 10px 0px 0px" }}
+      />
+      <StatusPlate nft={nft} />
+      <div className={styles["card-details-container"]}>
+        <div className={styles["card-details"]}>
+          <p>
+            <b>Owned by: </b>
+            {nft.token_seller == address
+              ? "YOU"
+              : (() => {
+                  const head = nft.token_seller.slice(0, 5);
+                  const tail = nft.token_seller.slice(address.length - 5);
+                  return `${head}...${tail}`;
+                })()}
+          </p>
+          <p>
+            <b>Collection: </b> {nft.name}
+          </p>
+          <p>
+            <b>Description: </b> {nft.description}
+          </p>
+          <p>
+            <b>Price: </b>
+            {Number(nft.token_price) / 1e18} ETH
+          </p>
         </div>
-        <div className={styles["card-overlay"]} />
       </div>
-    </Fragment>
+      <div className={styles["card-overlay"]} />
+    </div>
   );
 }
