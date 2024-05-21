535ed6e51abcafd8692bfe1ac05135428f520306
SKorchinskiy <ddphyk@gmail.com>
2024-05-21 07:28:03 +0300

    + add auctions provider to retrieve auctions

---
+++ client/src/app/providers/auctions.provider.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import {
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { AddressContext } from "./address.provider";
+import { NetworkContext } from "./network.provider";
+import useEnglishAuctionContract from "../hooks/useEnglishAuctionContract.hook";
+import useBlindAuctionContract from "../hooks/useBlindAuctionContract.hook";
+
+export type BlindAuction = {
+  auction_id: BigInt;
+  beneficiary: string;
+  nft_contract: string;
+  token_id: BigInt;
+  auction_end_time: BigInt;
+  highest_bidder: string;
+  highest_bid: BigInt;
+  is_blind: Boolean;
+};
+
+export type EnglishAuction = Exclude<BlindAuction, "is_blind">;
+
+export const AuctionsContext = createContext({
+  englishAuctions: [] as Array<EnglishAuction>,
+  blindAuctions: [] as Array<BlindAuction>,
+});
+
+export default function AuctionsProvider({ children }: PropsWithChildren) {
+  const [englishAuctions, setEnglishAuctions] = useState<Array<EnglishAuction>>(
+    []
+  );
+  const [blindAuctions, setBlindAuctions] = useState<Array<BlindAuction>>([]);
+
+  const { address } = useContext(AddressContext);
+  const { network } = useContext(NetworkContext);
+
+  const englishAuctionContract = useEnglishAuctionContract();
+  const blindAuctionContract = useBlindAuctionContract();
+
+  useEffect(() => {
+    const retrieveEnglishAuctions = async () => {
+      if (address && network && englishAuctionContract) {
+        const auctions = (await englishAuctionContract.methods
+          .get_all_auctions()
+          .call({
+            from: address,
+          })) as Array<EnglishAuction>;
+
+        console.log({ engAuc: auctions });
+
+        setEnglishAuctions(auctions);
+      }
+    };
+
+    retrieveEnglishAuctions();
+  }, [address, network, englishAuctionContract]);
+
+  useEffect(() => {
+    const retrieveBlindAuctions = async () => {
+      if (address && network && blindAuctionContract) {
+        const auctions = (await blindAuctionContract.methods
+          .get_all_auctions()
+          .call({
+            from: address,
+          })) as Array<BlindAuction>;
+
+        console.log({ engAuc: auctions });
+
+        setBlindAuctions(auctions);
+      }
+    };
+
+    retrieveBlindAuctions();
+  }, [address, network, blindAuctionContract]);
+
+  return (
+    <AuctionsContext.Provider value={{ englishAuctions, blindAuctions }}>
+      {children}
+    </AuctionsContext.Provider>
+  );
+}
