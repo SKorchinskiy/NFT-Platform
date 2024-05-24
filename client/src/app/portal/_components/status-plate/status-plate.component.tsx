3c645697ec0fe558ee1176b2425419b94c747d14
SKorchinskiy <ddphyk@gmail.com>
2024-05-25 00:05:17 +0300

    - retrieve status-plate styling into separate module

--- client/src/app/portal/_components/status-plate/status-plate.component.tsx
+++ client/src/app/portal/_components/status-plate/status-plate.component.tsx
@@ -1,24 +1,14 @@
 import { Status } from "../nft-card/nft-card.component";
 
+import styles from "./status-plate.module.css";
+
 const params = {
-  NONE: {
-    background: "black",
-  },
-  CANCELED: {
-    background: "yellow",
-  },
-  ACTIVE: {
-    background: "green",
-  },
-  SOLD: {
-    background: "red",
-  },
-  TRADE: {
-    background: "green",
-  },
-  ENDED: {
-    background: "red",
-  },
+  NONE: { background: "black" },
+  CANCELED: { background: "yellow" },
+  ACTIVE: { background: "green" },
+  SOLD: { background: "red" },
+  TRADE: { background: "green" },
+  ENDED: { background: "red" },
 } as {
   NONE: Object;
   CANCELED: Object;
@@ -31,9 +21,8 @@ const params = {
 export default function StatusPlate({ nft }: { nft: { status: BigInt } }) {
   return (
     <div
+      className={styles["status-plate-container"]}
       style={{
-        position: "absolute",
-        transform: "rotate(-30deg)",
         background: (
           params[
             Status[Number(nft.status)].toString() as
@@ -43,20 +32,8 @@ export default function StatusPlate({ nft }: { nft: { status: BigInt } }) {
               | "SOLD"
               | "TRADE"
               | "ENDED"
-          ] as {
-            background: string;
-          }
+          ] as { background: string }
         ).background,
-        color: "white",
-        padding: 5,
-        width: 170,
-        textAlign: "center",
-        zIndex: 1,
-        top: 16,
-        left: -20,
-        // borderRadius: 5,
-        boxShadow: "5px 5px 10px rgba(0, 0, 0, 0.3)",
-        clipPath: "polygon(9.8% 0%, 70% 0%, 98.5% 100%, 0.2% 100%)",
       }}
     >
       {Status[Number(nft.status)]}
