e99bf7b607189060b250a29d85cc6a965c230fa4
SKorchinskiy <ddphyk@gmail.com>
2024-05-26 21:32:38 +0300

    + add gcp secret manager client to files api route

--- client/src/app/api/files/route.ts
+++ client/src/app/api/files/route.ts
@@ -1,5 +1,6 @@
-import { PINATA_KEY } from "@/configs/constants";
 import { NextRequest, NextResponse } from "next/server";
+
+import { SecretManagerServiceClient } from "@google-cloud/secret-manager";
 
 async function createFile(url: string) {
   let response = await fetch(url);
@@ -18,14 +19,40 @@ export async function POST(request: NextRequest) {
     data.append("file", file);
     data.append("pinataMetadata", JSON.stringify({ name: data.get("name") }));
 
-    const res = await fetch("https://api.pinata.cloud/pinning/pinFileToIPFS", {
+    const gcpSecretClient = new SecretManagerServiceClient();
+    const [version] = await gcpSecretClient.accessSecretVersion({
+      name: 'projects/349442864224/secrets/PINATA_KEY/versions/latest'
+    });
+    
+    const PINATA_KEY = version.payload?.data?.toString() || '';
+
+    const pinnedImage = await fetch("https://api.pinata.cloud/pinning/pinFileToIPFS", {
       method: "POST",
       headers: { Authorization: `Bearer ${PINATA_KEY}` },
       body: data,
     });
-    const { IpfsHash } = await res.json();
 
-    return NextResponse.json({ IpfsHash }, { status: 200 });
+    const { IpfsHash } = await pinnedImage.json();
+    
+    const pinnedMetadata = await fetch(
+      "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${PINATA_KEY}`,
+        },
+        body: JSON.stringify({
+          token_id: Number(data.get('token_id')),
+          name: data.get('name'),
+          description: data.get('description'),
+          image: `ipfs://${IpfsHash}`,
+        }),
+      }
+    );
+    const { IpfsHash: IpfsCID } = await pinnedMetadata.json();
+
+    return NextResponse.json({ IpfsCID }, { status: 200 });
   } catch (error) {
     console.log(error);
     return NextResponse.json(
