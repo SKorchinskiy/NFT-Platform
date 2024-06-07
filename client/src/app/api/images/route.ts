11e29c30b2227f0d354e1f2ae705d2a95b902a04
SKorchinskiy <ddphyk@gmail.com>
2024-06-07 16:22:52 +0300

    + add replicate api token retrieval from gcp secret manager

--- client/src/app/api/images/route.ts
+++ client/src/app/api/images/route.ts
@@ -1,15 +1,20 @@
+import { SecretManagerServiceClient } from "@google-cloud/secret-manager";
 import { NextRequest, NextResponse } from "next/server";
 import Replicate from "replicate";
-import { REPLICATE_API_TOKEN } from "@/configs/constants";
-
-const replicate = new Replicate({
-  auth: REPLICATE_API_TOKEN,
-});
 
 export async function POST(request: NextRequest) {
   try {
     const data = await request.formData();
     const prompt: string = data.get("prompt") as string;
+
+    const gcpSecretClient = new SecretManagerServiceClient();
+    const [version] = await gcpSecretClient.accessSecretVersion({
+      name: 'projects/349442864224/secrets/REPLICATE_API_TOKEN/versions/latest'
+    });
+
+    const REPLICATE_API_TOKEN = version.payload?.data?.toString() || '';
+
+    const replicate = new Replicate({ auth: REPLICATE_API_TOKEN });
 
     const links = (await replicate.run(
       "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
